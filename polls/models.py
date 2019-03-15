0555f2e6c4123f93b27e007b700527f4104f93dc
Jared Freesh <jarbfree@gmail.com>
2019-03-14 22:57:38 -0500

    Added list display + sorts to Questions in Admin Model

--- polls/models.py
+++ polls/models.py
@@ -20,4 +20,12 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text    
+        return self.choice_text   
+        
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'         
